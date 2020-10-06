a28ed8b50c43caa335ee7c0a9d0c52c00e3a42a1
Aniket Jayateerth <jayateerth.aniket@gmail.com>
2020-10-06 10:05:37 +0530

    updated post request

--- src/server.py
+++ src/server.py
@@ -1,25 +1,25 @@
 import socket, sys
 import threading
 import os
-from response import generateResponse 
+from response import generateResponse
 
 #Ideally get this from the config file
 documentRoot = '/home/anup08/Desktop/CNProj/mhttp-server/src'
 resource = None
 f = None
+
+
 def matchAccept(headers):
     k = headers.split(',')
     # for i in k:
-        # print(i)
-
-
+    # print(i)
 
 
 def parse_GET_Request(headers):
     # TODO
     # Implement Conditional Get
     # Implement Range Header
-    # MIME Encoding response 
+    # MIME Encoding response
     params = {}
     for i in headers[1:]:
         try:
@@ -40,18 +40,18 @@ def parse_GET_Request(headers):
             path = documentRoot + path
         global resource
         global f
-        f = open(path,"r")
+        f = open(path, "r")
         resource = f.read()
         lastModified = os.path.getmtime(path)
         try:
             length = len(resource)
-        except :
+        except:
             pass
-        res = generateResponse(length,200,resource,lastModified)
-        return res 
+        res = generateResponse(length, 200, resource, lastModified)
+        return res
     except FileNotFoundError:
-        res = generateResponse(length,404)
-        return res 
+        res = generateResponse(length, 404)
+        return res
 
 
 def parse_POST_Request(headers):
@@ -63,7 +63,7 @@ def parse_POST_Request(headers):
 
     # For the purpose of the project, POST methods will write the incoming data into a logs file
 
-    params = {} 
+    params = {}
     body = []
     for i in headers[1:]:
 
@@ -75,27 +75,28 @@ def parse_POST_Request(headers):
 
     path = headers[0].split(' ')[1]
     path = documentRoot + path
-    
+
     if (path == "/"):
-        path = 'index.html'            
-        
+        path = 'index.html'
+
     # Check if file at path is write-able else respond with FORBIDDEN response
     if os.path.exists(path):
         if os.access(path, os.W_OK):
-            f1 = open(path, 'w')
+            f1 = open(path, 'w+')
             response_code = 200
         else:
             response_code = 403
     else:
-        f1 = open(path, 'w')
+        f1 = open(path, 'w+')
         response_code = 201
 
-    if response_code = 403:
+    if response_code == 403:
         res = generateResponse(403)
-        return res        
+        return res
 
-    f2 = open('./logs/post_log.txt')
-
+    f2 = open('./logs/post_log.txt', 'w+')
+    global resource
+    resource = f1.read()
     # Handle application/x-www-form-urlencoded type of data
     content_type = params['Content-Type']
     if "application/x-www-form-urlencoded" in content_type:
@@ -108,14 +109,10 @@ def parse_POST_Request(headers):
                 param = param.split('=')
                 form_data[param[0]] = param[1]
 
-        f2.write(form_data)
+        f2.write(str(form_data))
 
-    res = generateResponse(response_code)
+    res = generateResponse(len(resource), response_code, resource, None)
     return res
-
-
-
-
 
 
 def parse_PUT_Request(headers):
@@ -135,8 +132,8 @@ def process(data):
         headers = [i for i in data.split('\n')]
         tokens = headers[0].split(' ')
         method = tokens[0]
-        
-        if(method == 'GET'):
+
+        if (method == 'GET'):
             return parse_GET_Request(headers)
         elif (method == 'POST'):
             parse_POST_Request(headers)
@@ -150,7 +147,7 @@ def process(data):
     except e:
         print(e)
         print("Return 400 Bad request")
-        return generateResponse(0,400)
+        return generateResponse(0, 400)
 
 
 if __name__ == "__main__":
@@ -169,16 +166,15 @@ if __name__ == "__main__":
                 data = clientsocket.recv(5000).decode('utf-8')
                 # print(data)
                 res = process(data)
-                if('\r\n\r\n' in data):
+                if ('\r\n\r\n' in data):
                     break
-            
+
             print(res)
             clientsocket.send(res.encode('utf-8'))
-            
+
         except e:
             print(e)
             print("err")
         finally:
             clientsocket.close()
             f.close()
-
