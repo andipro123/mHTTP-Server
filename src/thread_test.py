d1acae2acfb7c25fee902f10e4d2059c151ef5fb
Aniket Jayateerth <jayateerth.aniket@gmail.com>
2020-10-09 10:07:03 +0530

    bugfix

--- src/thread_test.py
+++ src/thread_test.py
@@ -1,16 +1,18 @@
 # from concurrent.futures import ThreadPoolExecutor
 import requests
 import threading
+import time
 
 
 # def get_url(url):
 #     return requests.get(url)
 def send_request(url):
     print(requests.get(url))
+    time.sleep(10)
     return
 
 
-n = 1000
+n = 10
 
 while (n):
     client_thread = threading.Thread(target=send_request,
