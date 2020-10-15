f711d29ae731b3996600cf634512201e94634a02
Aniket Jayateerth <jayateerth.aniket@gmail.com>
2020-10-15 11:51:31 +0530

    merged new content

--- src/methods/get.py
+++ src/methods/get.py
@@ -2,14 +2,23 @@ import os
 import sys
 sys.path.append(os.path.abspath(os.path.join('..')))
 from utils import entityHeaders
-from response import generateResponse
+from response import *
 from logger import Logger
 import pathlib
+from utils.mediaTypes import mediaTypes
+import random
 
 documentRoot = str(pathlib.Path().absolute()) + "/assets/"
 logger = Logger()
 
+def getExtension(mediaTypes):
+    f = {}
+    for k in mediaTypes.keys():
+        f[mediaTypes[k]] = k
+    return f
 
+def generateEtag(time,length):
+    return str(int(time)) + str(length)
 def matchAccept(headers):
     k = headers.split(',')
     par = []
@@ -37,52 +46,75 @@ def parse_GET_Request(headers,method=""):
     par = matchAccept(params['Accept'])
     path = headers[0].split(' ')[1]
     length = 0
+    ctype = "text/html"
     try:
         if (path == "/"):
             path = documentRoot + 'index.html'
         else:
-            path = documentRoot + path
+            try:
+                extension ='.' + path.split('.')[1]
+                ctype = getExtension(mediaTypes)[extension]
+                path = documentRoot + path
+            except:
+                ctype = par[0]
         f = open(path, "rb")
         resource = f.read()
         lastModified = os.path.getmtime(path)
+        Etag = generateEtag(lastModified,len(resource))
         try:
             length = len(resource)
         except:
             pass
-        #415
-        if('Content-Encoding' in params.keys() and params['Content-Encoding'] not in entityHeaders['Content-Encoding']):
-            res = generateResponse(0, 415)
-            print(res)
-            f.close()
-            return res, ""
-        #200
-        if('Content-Encoding' in params.keys() and params['Content-Encoding'] == 'gzip'):
-            res = generateResponse(length, 200, resource, lastModified, par[0])
-            res = res[:len(res) - 2] + 'Content-Encoding: gzip' + '\r\n\r\n'
-            print(res)
-            f.close()
-            return res, resource
-        #head
+        reqParams = {
+            'length' : length,
+            'code' : 200,
+            'ctype' : ctype,
+            'etag' : Etag,
+        }
         if (method == "HEAD"):
-            res = generateResponse(length, 200, resource, lastModified, par[0],
-                                   "HEAD")
+            # res = generateResponse(length, 200, resource, lastModified, ctype,"HEAD")
+            res = generateGET(reqParams)
             print(res)
             return res, ""
         else:
-            res = generateResponse(length, 200, resource, lastModified, par[0])
+            #415
+            if('Content-Encoding' in params.keys() and params['Content-Encoding'] not in entityHeaders['Content-Encoding']):
+                res = generateResponse(0, 415)
+                f.close()
+                return res, ""
+            res = generateGET(reqParams)
+            # res = generateResponse(length, 200, resource, lastModified, ctype,Etag)
+        
+        if('If-None-Match' in params.keys()):
+            # e = getEtag(f)
+            e = Etag
+            if(e == params['If-None-Match']):
+                reqParams['code'] = 0
+                reqParams['length'] = 0
+                return generateGET(reqParams), ""
+                # return generateResponse(0, 304, resource, lastModified, ctype),""
+                
+        
+        #Successfull Content Encoding
+        if('Content-Encoding' in params.keys() and params['Content-Encoding'] == 'gzip'):
+            res = res[:len(res) - 2] + 'Content-Encoding: gzip' + '\r\n\r\n'
+        #Check at end
+        if('Accept-Ranges' in params.keys()):
+            k = int(params['Accept-Ranges'])
+            resRange = resource[:k]
+            res = res[:len(res) - 2] + 'Accept-Ranges: {}'.format(k) + '\r\n\r\n'
+            resource = resRange
+        
         logger.generate(headers[0], res)
         print(res)
         f.close()
         return res , resource
+
     except FileNotFoundError:
-        res = generateResponse(length, 404)
+        reqParams['code'] = 404
+        reqParams['length'] = 0
+        # res = generateResponse(length, 404)
+        res = generateGET(reqParams)
         logger.generate(headers[0], res)
         return res, ""
 
-
-def parse_HEAD_Request(headers):
-    # Returns the response of GET without the message body
-    # TODO
-    # Add more headers to the repsonse in the reponse.py file
-    # Handle Caching with GET
-    return parse_GET_Request(headers, "HEAD")
--- src/utils/mediaTypes.py
+++ src/utils/mediaTypes.py
@@ -7,12 +7,16 @@ mediaTypes = {
     "text/html" : ".html",
     "image/vnd.microsoft.icon" : ".ico",
     "image/jpeg" : ".jpg",
-    "application/json" : ".js",
+    "image/aviff" : ".png",
+    "test/javascript" : ".js",
     "application/json" : ".json",
     "audio/mpeg" : ".mp3",
     "video/mpeg" : ".mpeg",
-    "application/x-httpd-php" : ".php"
+    "application/x-httpd-php" : ".php",
+    "application/pdf": ".pdf"
 }
+
+
 
 
 
